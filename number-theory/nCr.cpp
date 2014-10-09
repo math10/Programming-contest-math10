956c0c6d047cc3603afabd226af78bd1d65cba71
MD. Musfiqur Rahman Sanim <sanim_10@yahoo.com>
2014-10-09 15:54:48 +0600

    Update nCr.cpp

--- number-theory/nCr.cpp
+++ number-theory/nCr.cpp
@@ -1,8 +1,8 @@
-
-int64 nCr[1010][1010];
+const int MAX = 1000 + 7;
+int64 nCr[MAX][MAX];
 void make_nCr(){
      nCr[0][0] = 1;
-        For(i,1000) {
+        For(i,MAX-7) {
                 nCr[i][0] = 1;
                 For(j,i) {
                         nCr[i][j] = (nCr[i-1][j-1] + nCr[i-1][j]);
